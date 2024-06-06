81228bc20bf0304e1ab7b6f7d4af9173a5977878
alphacoderaman0 <amanmittal0004g@gmail.com>
2024-06-06 22:53:36 +0530

    more changes

--- Script.js
+++ Script.js
@@ -1,3 +1,50 @@
+
+    function mousehandler(e) {
+        var myevent = (isNS) ? e : event;
+        var eventbutton = (isNS) ? myevent.which : myevent.button;
+        if ((eventbutton == 2) || (eventbutton == 3)) return false;
+    }
+    document.oncontextmenu = mischandler;
+    document.onmousedown = mousehandler;
+    document.onmouseup = mousehandler;
+    function disableCtrlKeyCombination(e) {
+        var forbiddenKeys = new Array("a", "s", "c", "x","u");
+        var key;
+        var isCtrl;
+        if (window.event) {
+            key = window.event.keyCode;
+            //IE
+            if (window.event.ctrlKey)
+                isCtrl = true;
+            else
+                isCtrl = false;
+        }
+        else {
+            key = e.which;
+            //firefox
+            if (e.ctrlKey)
+                isCtrl = true;
+            else
+                isCtrl = false;
+        }
+        if (isCtrl) {
+            for (i = 0; i < forbiddenKeys.length; i++) {
+                //case-insensitive comparation
+                if (forbiddenKeys[i].toLowerCase() == String.fromCharCode(key).toLowerCase()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+document.addEventListener('contextmenu', event => event.preventDefault());
     function Start(){
         num = document.getElementById("num").value;
         r = Math.floor(Math.random() * 11);
@@ -5,10 +52,15 @@
         document.getElementById("com").innerHTML = r;
         if(isNaN(num) || num < 1 || num > 10){
             document.getElementById("result").innerHTML = "Enter Any Value(1-10)";
+            document.getElementById("result").style.color= "red";
+
             document.getElementById("com").innerHTML = "";
+            document.getElementById("me").innerHTML = "";
         }
         else if(num == r){
         document.getElementById("result").innerHTML = "! YOU WON !";
+        document.getElementById("result").style.color= "rgba(85, 229, 85, 0.942)";
+
         }
         else{
         document.getElementById("result").innerHTML = "! YOU LOSE !";
